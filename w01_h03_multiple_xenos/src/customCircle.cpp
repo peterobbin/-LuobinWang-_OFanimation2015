4b5d0966337e3372f4ef6b50cb5448d1d96131ca
peterobbin <beijingwang@gmail.com>
2015-02-08 21:45:46 -0500

    h03 start

---
+++ w01_h03_multiple_xenos/src/customCircle.cpp
@@ -0,0 +1,9 @@
+//
+//  customCircle.cpp
+//  w01_h03_multiple_xenos
+//
+//  Created by peterobbin on 2/8/15.
+//
+//
+
+#include "customCircle.h"
---
+++ w01_h03_multiple_xenos/src/customCircle.h
@@ -0,0 +1,20 @@
+//
+//  customCircle.h
+//  w01_h03_multiple_xenos
+//
+//  Created by peterobbin on 2/8/15.
+//
+//
+
+#pragma once
+#include "ofMain.h"
+
+
+class customCircle {
+    customCircle();
+    void setup(ofVec2f _initPos);
+    void update(ofVec2f _mouse);
+    void draw();
+
+
+};
